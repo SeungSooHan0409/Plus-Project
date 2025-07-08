555259e180086a3c816bbca0690d7a90fb3d6eef
SEUNGSOOHAN <tmdtn4110@gmail.com>
2025-07-08 15:17:15 +0900

    feat : 예약 생성 API 작업완료

--- src/main/java/com/example/plusproject/domain/reservation/controller/ReservationController.java
+++ src/main/java/com/example/plusproject/domain/reservation/controller/ReservationController.java
@@ -1,8 +1,12 @@
 package com.example.plusproject.domain.reservation.controller;
 
+import com.example.plusproject.domain.reservation.dto.RequestDto;
+import com.example.plusproject.domain.reservation.dto.ResponseDto;
 import com.example.plusproject.domain.reservation.service.ReservationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +17,16 @@ public class ReservationController {
 
     private final ReservationService reservationService;
 
+    // 숙소 예약 API - 추후 토큰에서 정보추출 필요
+    @PostMapping
+    public ResponseEntity<ResponseDto> createReservation(
+            @RequestBody RequestDto requestDto
+            ) {
 
+        return ResponseEntity.ok(
+                reservationService.reserveAccommodation(requestDto.getGuestCount(), requestDto.getCheckInDate(), requestDto.getAccommodationAddress())
+        );
+
+    }
 
 }
--- src/main/java/com/example/plusproject/domain/reservation/service/ReservationService.java
+++ src/main/java/com/example/plusproject/domain/reservation/service/ReservationService.java
@@ -1,13 +1,69 @@
 package com.example.plusproject.domain.reservation.service;
 
+import com.example.plusproject.domain.accommodation.entity.Accommodation;
+import com.example.plusproject.domain.reservation.dto.ReservationData;
+import com.example.plusproject.domain.reservation.dto.ResponseDto;
+import com.example.plusproject.domain.reservation.entity.Reservation;
 import com.example.plusproject.domain.reservation.repository.ReservationRepository;
+import com.example.plusproject.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
 public class ReservationService {
 
     private final ReservationRepository reservationRepository;
+    // Accommodation 과 merge 하면 주석 해제
+//    private final AccommodationServcie accommodationServcie;
+
+    // 숙소예약 메서드
+    public ResponseDto reserveAccommodation(Long guestCount, LocalDate checkInDate, String accommodationAddress) {
+
+        // 임의 유저 생성 - User 와 merge 하면 토큰에서 유저정보 추출하기
+        User user = new User(
+                "name",
+                "nickname",
+                "email",
+                "password",
+                "phoneNumber",
+                "residence",
+                "role"
+        );
+
+        // 임의 숙소 생성 - Accommodation 과 merge 하면 필드에서 숙소주소로 숙소 불러오기
+        Accommodation accommodation = new Accommodation(
+                "address",
+                "city",
+                "description",
+                "roomType",
+                "services",
+                10000,
+                LocalDateTime.now(),
+                user
+        );
+
+        // 예약 생성
+        Reservation reservaion = new Reservation(accommodation, user, checkInDate, guestCount);
+
+        // 예약 저장
+        reservationRepository.save(reservaion);
+
+        // data 생성
+        ReservationData data = new ReservationData(
+                reservaion.getId(),
+                reservaion.getAccommodation().getAddress(),
+                reservaion.getGuestCount(),
+                reservaion.getCheckInDate());
+
+        // Response 생성
+        ResponseDto responseDto = new ResponseDto(true, "예약 성공!", data, LocalDateTime.now());
+
+        return responseDto;
+
+    }
 
 }
