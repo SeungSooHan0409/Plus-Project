23f2ae9f1226fa8ca924a4b8b816889856aa29db
SEUNGSOOHAN <tmdtn4110@gmail.com>
2025-07-09 16:54:32 +0900

    fix : 불필요한 예외삭제

--- src/main/java/com/example/plusproject/domain/reservation/exception/ReservationNotFound.java
+++
@@ -1,7 +0,0 @@
-package com.example.plusproject.domain.reservation.exception;
-
-public class ReservationNotFound extends RuntimeException {
-    public ReservationNotFound(String message) {
-        super(message);
-    }
-}
