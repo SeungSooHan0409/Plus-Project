2f988a0e59ce92a18b624c9edbdaf1ee9000cd40
SEUNGSOOHAN <tmdtn4110@gmail.com>
2025-07-10 18:11:55 +0900

    feat : 찜하기 (post) api

--- src/main/java/com/example/plusproject/domain/favorite/controller/FavoriteController.java
+++ src/main/java/com/example/plusproject/domain/favorite/controller/FavoriteController.java
@@ -1,9 +1,12 @@
 package com.example.plusproject.domain.favorite.controller;
 
+import com.example.plusproject.common.dto.ApiResponseDto;
+import com.example.plusproject.config.CustomUserPrincipal;
 import com.example.plusproject.domain.favorite.service.FavoriteService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,5 +14,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class FavoriteController {
 
     private final FavoriteService favoriteService;
+
+
+    // 찜하기
+    @PostMapping("/{accommodationId}")
+    public ResponseEntity<ApiResponseDto> createFavorite(
+            @PathVariable Long accommodationId,
+            @AuthenticationPrincipal CustomUserPrincipal principal
+            ) {
+
+        // userId 조회
+        Long userId = principal.getId();
+
+        return ResponseEntity.ok(favoriteService.createFavorite(accommodationId, userId));
+
+    }
 
 }
--- src/main/java/com/example/plusproject/domain/favorite/service/FavoriteService.java
+++ src/main/java/com/example/plusproject/domain/favorite/service/FavoriteService.java
@@ -1,9 +1,43 @@
 package com.example.plusproject.domain.favorite.service;
 
+import com.example.plusproject.common.dto.ApiResponseDto;
+import com.example.plusproject.domain.accommodation.entity.Accommodation;
+import com.example.plusproject.domain.accommodation.service.AccommodationService;
+import com.example.plusproject.domain.favorite.dto.FavoriteData;
+import com.example.plusproject.domain.favorite.entity.Favorite;
+import com.example.plusproject.domain.favorite.repository.FavoriteRepository;
+import com.example.plusproject.domain.user.entity.User;
+import com.example.plusproject.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class FavoriteService {
+
+    private final FavoriteRepository favoriteRepository;
+    private final UserService userService;
+    private final AccommodationService accommodationService;
+
+    // 찜하기 메서드
+    public ApiResponseDto createFavorite(Long accommodationId, Long userId) {
+
+        // 찜하려는 유저 조회
+        User user = userService.findUserById(userId);
+
+        // 찜하려는 숙소 조회
+        Accommodation accommodation = accommodationService.findAccommodationById(accommodationId);
+
+        // 찜 생성 및 저장
+        Favorite favorite = new Favorite(user, accommodation);
+        favoriteRepository.save(favorite);
+
+        // data 생성
+        FavoriteData data = new FavoriteData(favorite.getAccommodation().getAccommodationName(), favorite.getUser().getNickname());
+
+        // 응답 반환
+        return ApiResponseDto.success("찜하기 성공!", data);
+
+    }
+
 }
