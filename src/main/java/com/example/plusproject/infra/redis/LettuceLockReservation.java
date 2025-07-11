63a70db7cdaaedfc9ed62151e3eada4094996dc9
doohaaa <engk5423@naver.com>
2025-07-11 20:51:49 +0900

    feat: redis 활용한 분산락 구현 (숙소 예약시)

---
+++ src/main/java/com/example/plusproject/infra/redis/LettuceLockReservation.java
@@ -0,0 +1,80 @@
+package com.example.plusproject.infra.redis;
+
+
+// spin lock 형태
+
+import com.example.plusproject.domain.accommodation.entity.Accommodation;
+import com.example.plusproject.domain.accommodation.service.AccommodationService;
+import com.example.plusproject.domain.reservation.service.ReservationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.LinkedList;
+import java.util.Queue;
+
+@Service
+@RequiredArgsConstructor
+public class LettuceLockReservation {
+
+    private final RedisLockRepository redisLockRepository;
+    private final ReservationService reservationService;
+    private final AccommodationService accommodationService;
+
+
+    public void makingReservation(Long guestCount, LocalDate checkInDate, LocalDate checkoutDate, String accommodationAddress, Long userId) {
+
+        // 숙소 조회
+        Accommodation accommodation = accommodationService.findAccommodationByAddress(accommodationAddress);
+
+        // lock 을 위한 '숙소이름 + 숙박 날짜'가 합해진 key 생성
+        // ex. key 값 ) 롯데호텔1101호:20250711
+        // 큐에 키 값 담아 놓고 하나씩 빼서 모두 lock 걸어줌
+        Queue<String> keys = new LinkedList<>();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+
+        for(LocalDate date = checkInDate; date.isBefore(checkoutDate); date = date.plusDays(1)){
+            String tmpKey = accommodation.getAccommodationName()+":"+date.format(formatter);
+            keys.offer(tmpKey);
+        }
+
+        while(getLocks(keys)){                                    // lock 못잡으면 무한 대기
+            try{
+                Thread.sleep(100);                          // lock 획득 못했으면 100ms 후 다시 시도
+            } catch (InterruptedException e){
+                throw new RuntimeException(e);
+            }
+        }
+
+        // lock 을 획득 한 뒤에는
+        try {                                                      // lock 잡고 while 문 벗어났다면 예약 확정
+            reservationService.reserveAccommodation(guestCount, checkInDate, checkoutDate, accommodationAddress, userId);
+        } finally{
+
+            releaseLocks(keys);
+        }
+    }
+
+
+    // 여러개 lock 메서드
+    private Boolean getLocks(Queue<String> keys){
+        while(!keys.isEmpty()){
+            String element = keys.poll();
+            redisLockRepository.lock(element);
+        }
+        return true;
+    }
+
+    // 여러개 unlock 메서드
+    private Boolean releaseLocks(Queue<String> keys){
+        while(!keys.isEmpty()){
+            String element = keys.poll();
+            redisLockRepository.unlock(element);
+        }
+        return true;
+    }
+
+
+
+}
---
+++ src/main/java/com/example/plusproject/infra/redis/RedisLockRepository.java
@@ -0,0 +1,29 @@
+package com.example.plusproject.infra.redis;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.time.Duration;
+
+@Repository
+public class RedisLockRepository {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public RedisLockRepository(RedisTemplate<String, String> redisTemplate){
+        this.redisTemplate = redisTemplate;
+    }
+
+    public Boolean lock(String key){
+        return redisTemplate
+                .opsForValue()                                                  // String 을 serialise/deserialise 하기 위한 메서드
+                .setIfAbsent(key,"lock", Duration.ofMillis(3000));        // key,"lock" 으로 key-value 값 저장을 시도, 3초 동안만 락을 유지
+                                                                                // 이 키는 3초 후에 삭제됨. 왜냐면 락을 걸었는데 처리도중 어플리케이션이 죽으면 락 못풀고 키가 영원히 남음 -> deadlock 발생
+    }
+
+    public Boolean unlock(String key){
+        return redisTemplate.delete(key);
+    }
+
+}
