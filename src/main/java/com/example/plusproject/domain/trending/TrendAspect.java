0198cfb2906434ceea2b49c50e39e8134e074ad0
saevit <woosaevit@gmail.com>
2025-07-11 09:56:03 +0900

    fix: search rename에 따른 execution() 수정

--- src/main/java/com/example/plusproject/domain/trending/TrendAspect.java
+++ src/main/java/com/example/plusproject/domain/trending/TrendAspect.java
@@ -20,7 +20,7 @@ public class TrendAspect {
 
 	private final RedisTemplate<String, String> redisTemplate; // RedisTemplate을 사용하여 Redis와 통신
 
-	@AfterReturning("execution(* com.example.plusproject..AccommodationController.searchAccommodations(..))")
+	@AfterReturning("execution(* com.example.plusproject..AccommodationController.searchAccommodationsV1(..))")
 	public void searchLogging(JoinPoint joinPoint) {
 		Object[] args = joinPoint.getArgs();
 
