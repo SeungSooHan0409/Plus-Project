968b81d9ecc4205d5d5bec80723b665d8bff341d
doohaaa <engk5423@naver.com>
2025-07-14 20:33:34 +0900

    fix: 숙소 조회 로직 api 수정에 따른 spring security 관련 코드 수정

--- src/main/java/com/example/plusproject/config/SecurityConfig.java
+++ src/main/java/com/example/plusproject/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/auth/signup","/api/auth/login").permitAll()
                         .requestMatchers(HttpMethod.GET,
                                 "/api/reviews*",
-                                "/api/accommodations/search*", "/api/accomodations/search*", "/api/accomodations/city*",
+                                "/api/accommodations/v3/search/keyword*", "/api/accommodations/v3/search/city*",
                                 "/api/trending*").permitAll()   // 조회 로직 - 로그인 불필요
                         .anyRequest().authenticated()
                 )
