f62b405c7858478e67c1bdb654404bfcd52ac979
SEUNGSOOHAN <tmdtn4110@gmail.com>
2025-07-14 12:54:02 +0900

    fix : 기본 생성자 추가

--- src/main/java/com/example/plusproject/domain/favorite/dto/FavoriteData.java
+++ src/main/java/com/example/plusproject/domain/favorite/dto/FavoriteData.java
@@ -1,8 +1,10 @@
 package com.example.plusproject.domain.favorite.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class FavoriteData {
 
     private String accommodationName;
